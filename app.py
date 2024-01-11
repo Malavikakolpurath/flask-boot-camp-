228fcaa3add65a6afc648f649423bdbdbb8091f0
Malavikakolpurath <141305577+Malavikakolpurath@users.noreply.github.com>
2024-01-11 11:16:11 +0000

    3

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
+
+@app.route('/')
+def hello():
+    return ('Hi')
+
+@app.route('/name')
+def hey():
+    return ('How are you')
+
+
+if __name__ =='__main__':
+    app.run()
